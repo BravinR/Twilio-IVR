a39341aa2341cbd9305070f856491620e59b9aa2
BravinR <bravinrutoh@gmail.com>
2021-02-18 21:05:35 -0600

    changed settings.py

--- ivr/ivr/settings.py
+++ ivr/ivr/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1!p+vi$^zya@xhv7@-q(lwff)nefcm*wj-+jwl0t3+0-0ebv$m'
+SECRET_KEY = 'your secret key here'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
